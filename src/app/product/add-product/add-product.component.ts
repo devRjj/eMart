a3cdd3b3a60b728bec529c646d5810798a1ee3de
Devraj Singh <devrajmac@Devrajs-MacBook-Air.local>
2023-06-29 19:29:32 +0530

    pretty applied

--- src/app/product/add-product/add-product.component.ts
+++ src/app/product/add-product/add-product.component.ts
@@ -7,141 +7,129 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-add-product',
   templateUrl: './add-product.component.html',
-  styleUrls: ['./add-product.component.scss']
+  styleUrls: ['./add-product.component.scss'],
 })
 export class AddProductComponent {
-  selectedId:string|null=null;
-  addproducts!:FormGroup;
-  isviesavailable:boolean=false;
+  selectedId: string | null = null;
+  addproducts!: FormGroup;
+  isviesavailable: boolean = false;
   isUpdate: boolean = false;
-  isEditable!:any;
-  viewProduct!:any;
-  backButton!:any;
-  viewselectedid:any
-  DateCreate=new Date()
-    constructor(private el:FormBuilder,private psotdata:ProductService,private route:ActivatedRoute,private router:Router,public dialog:  MatDialog,){}
-    ngOnInit(): void {
-     
-      this.createproduct();
-      this.selectedId = this.route.snapshot.paramMap.get("id");
-      console.log("selected Id", this.selectedId);
+  isEditable!: any;
+  viewProduct!: any;
+  backButton!: any;
+  viewselectedid: any;
+  DateCreate = new Date();
+  constructor(
+    private el: FormBuilder,
+    private psotdata: ProductService,
+    private route: ActivatedRoute,
+    private router: Router,
+    public dialog: MatDialog
+  ) {}
+  ngOnInit(): void {
+    this.createproduct();
 
-      this.viewselectedid=this.route.snapshot.queryParamMap.get('id');
-      console.log("selected id",this.viewselectedid);
-      this.isEditable = this.route.snapshot.queryParamMap.get('temp');
-      console.log(this.isEditable);
-      this.viewProduct = this.route.snapshot.queryParamMap.get('productname');
-      console.log(this.viewProduct);
-      
-      this.backButton = this.route.snapshot.queryParamMap.get('flag');
-      console.log(this.backButton);
-    
-      if(this.selectedId)
-        {
-          this.isUpdate = true;
-           alert("Do you Want to Edit product. ");
-          this.getListforUpdation();
-        }else if(this.isEditable){
-          
-        
-          this.ViewProductDetails();
-          this.addproducts.patchValue(this.viewProduct)
-        }
-  
+    this.selectedId = this.route.snapshot.paramMap.get('id');
+    console.log('selected Id', this.selectedId);
+
+    this.viewselectedid = this.route.snapshot.queryParamMap.get('id');
+    console.log('selected id', this.viewselectedid);
+
+    this.isEditable = this.route.snapshot.queryParamMap.get('temp');
+    console.log(this.isEditable);
+
+    this.viewProduct = this.route.snapshot.queryParamMap.get('productname');
+    console.log(this.viewProduct);
+
+    this.backButton = this.route.snapshot.queryParamMap.get('flag');
+    console.log(this.backButton);
+
+    if (this.selectedId) {
+      this.isUpdate = true;
+      alert('Do you Want to Edit product. ');
+      this.getListforUpdation();
+    } else if (this.isEditable) {
+      this.ViewProductDetails();
+      this.addproducts.patchValue(this.viewProduct);
     }
-    createproduct(){
-      this.addproducts=this.el.group({
-        productname:['',[]],
-        Discription:['',[]],
-        Category:['',[]],
-        Brand:['',[]],
-        Status:['',[]],
-        Sizes:['',[]],
-        Colors:['',[]],
-        Tags:['',[]],
-        ProductImage:['',[]],
-        ProductCode:['',[]],
-        ProductSKU:['',[]],
-        Gender:['',[]],
-        quantity:['',[]],
-        RegualrPrice:['',[]],
-        SalePrice:['',[]],
-        Date:['',[]]
-  
-      })
-    }
-    SaveData(){
-      if(this.addproducts.valid)
-      {
-        const body=this.addproducts.value;
-    
-       this.psotdata.PostDataToServer("product",body).subscribe((res:any)=>
-       {
-         console.log(res);
-         alert("product Created Sucessfully" );
-         this.router.navigate(['/product/product-list']);
-        },error=>{  })
-       
-      }
-    }
-  
-   getListforUpdation()
-   {
-    const endpoint="product/"+this.selectedId;
-    this.psotdata.GetDataToServer(endpoint).subscribe((res:any)=>
-    {
-    //  console.log(res);
-     this.addproducts.patchValue(res);
-  
-    })
+  }
+
+  createproduct() {
+    this.addproducts = this.el.group({
+      productname: ['', []],
+      Discription: ['', []],
+      Category: ['', []],
+      Brand: ['', []],
+      Status: ['', []],
+      Sizes: ['', []],
+      Colors: ['', []],
+      Tags: ['', []],
+      ProductImage: ['', []],
+      ProductCode: ['', []],
+      ProductSKU: ['', []],
+      Gender: ['', []],
+      quantity: ['', []],
+      RegualrPrice: ['', []],
+      SalePrice: ['', []],
+      Date: ['', []],
+    });
   }
   
-  
-  
-  
-  Submit()
-  {
-    if(this.selectedId ==null)
-    {
+  SaveData() {
+    if (this.addproducts.valid) {
+      const body = this.addproducts.value;
+
+      this.psotdata.PostDataToServer('product', body).subscribe(
+        (res: any) => {
+          console.log(res);
+          alert('product Created Sucessfully');
+          this.router.navigate(['/product/product-list']);
+        },
+        (error) => {}
+      );
+    }
+  }
+
+  getListforUpdation() {
+    const endpoint = 'product/' + this.selectedId;
+    this.psotdata.GetDataToServer(endpoint).subscribe((res: any) => {
+      //  console.log(res);
+      this.addproducts.patchValue(res);
+    });
+  }
+
+  Submit() {
+    if (this.selectedId == null) {
       this.SaveData();
-    }
-    else
-    {
+    } else {
       this.updateData();
-   }
-  }
-  
-  
-  
-  
-    updateData()
-    {
-      const endPoint = "product/" + this.selectedId;
-  
-      this.psotdata.UpdateDataToServer(endPoint, this.addproducts.value).subscribe((resp: any) =>
-      {
-        // console.log(resp);
-        alert("product Getting Updated SuccessFully...!");
-        
-        this.router.navigate(['/product/product-list']);
-      },
-        error => 
-        {
-          console.log("product not getting updated");
-        })
     }
-  
-    ViewProductDetails(){
-      const endPoint = "product/" + this.viewselectedid
-      this.psotdata.GetDataToServer(endPoint).subscribe((response:any)=>{
-        
-        console.log(response)
-       
-        this.addproducts.patchValue(response)
-  
-       
-      })
-  
+  }
+
+  updateData() {
+    const endPoint = 'product/' + this.selectedId;
+
+    this.psotdata
+      .UpdateDataToServer(endPoint, this.addproducts.value)
+      .subscribe(
+        (resp: any) => {
+          // console.log(resp);
+          alert('product Getting Updated SuccessFully...!');
+
+          this.router.navigate(['/product/product-list']);
+        },
+        (error) => {
+          console.log('product not getting updated');
+        }
+      );
+  }
+
+  ViewProductDetails() {
+    const endPoint = 'product/' + this.viewselectedid;
+    this.psotdata.GetDataToServer(endPoint).subscribe((response: any) => {
+      console.log(response);
+
+      this.addproducts.patchValue(response);
+    });
   }
 }
-
--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -7,59 +7,62 @@ import { MatTableDataSource } from '@angular/material/table';
 @Component({
   selector: 'app-product-list',
   templateUrl: './product-list.component.html',
-  styleUrls: ['./product-list.component.scss']
+  styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent {
-  displayedColumns: string[] = ['Product', 'Created', 'Status', 'Rating','price','Feactured','Actions'];
-  dataSource!: MatTableDataSource<any>; ;
- 
- TodaysDate=new Date()
- constructor(private productlistservice:ProductService,private route:Router,public dialog: MatDialog) {
+  displayedColumns: string[] = [
+    'Product',
+    'Created',
+    'Status',
+    'Rating',
+    'price',
+    'Feactured',
+    'Actions',
+  ];
 
+  dataSource!: MatTableDataSource<any>;
+
+  TodaysDate = new Date();
+  constructor(
+    private productlistservice: ProductService,
+    private route: Router,
+    public dialog: MatDialog
+  ) {}
+
+  ngOnInit() {
+    this.productlistadded();
   }
- ngOnInit() {
-    
-    this.productlistadded()
-    
-  }
-  productlistadded(){
-    this.productlistservice.GetDataToServer("product").subscribe((el:any)=>{
-      console.log(el)
-      this.dataSource=new MatTableDataSource(el);;
-      
-    })
+
+  productlistadded() {
+    this.productlistservice.GetDataToServer('product').subscribe((el: any) => {
+      console.log(el);
+      this.dataSource = new MatTableDataSource(el);
+    });
   }
 
- OnDelete(rowData: any) 
-  {
-    alert("Do you really want to delete  ? Category will be Deleted permanently.")
-   
-     // console.log(rowData);
-    let selectedIndex = this.dataSource.data.findIndex((el: any) => el.id === rowData.id);
-    console.log("selectedIndex", selectedIndex);
-   
-    const endpoint = "product/" + rowData.id;
-    this.productlistservice.DeleteDataToServer(endpoint).subscribe((res: any) => 
-    {
-      console.log(res);
-    
-      this.dataSource.data.splice(selectedIndex, 1);
-      console.log("product Deleted SucessFully...!", this.dataSource);
+  OnDelete(rowData: any) {
+    alert(
+      'Do you really want to delete  ? Category will be Deleted permanently.'
+    );
+    let selectedIndex = this.dataSource.data.findIndex(
+      (el: any) => el.id === rowData.id
+    );
+    console.log('selectedIndex', selectedIndex);
 
-      alert(" Deleted SucessFully...!");
-      this.dataSource._updateChangeSubscription();
-    },
-      error => 
-      {
-        console.log(" product not deleted....!!")
-      })
+    const endpoint = 'product/' + rowData.id;
+    this.productlistservice.DeleteDataToServer(endpoint).subscribe(
+      (res: any) => {
+        console.log(res);
 
-    }
- 
+        this.dataSource.data.splice(selectedIndex, 1);
+        console.log('product Deleted SucessFully...!', this.dataSource);
 
-
-
+        alert(' Deleted SucessFully...!');
+        this.dataSource._updateChangeSubscription();
+      },
+      (error) => {
+        console.log(' product not deleted....!!');
+      }
+    );
+  }
 }
-
-
-
