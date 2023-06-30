2526538bcb467e9b422d61cf486e1ac96d8a86fd
Devraj Singh <devrajmac@Devrajs-MacBook-Air.local>
2023-06-30 16:32:35 +0530

    pretty_applied

--- src/app/product/product-list/product-list.component.ts
+++ src/app/product/product-list/product-list.component.ts
@@ -10,67 +10,23 @@ import { AllproductserviceService } from 'src/app/shared/services/allproductserv
   styleUrls: ['./product-list.component.scss'],
 })
 export class ProductListComponent {
-  displayedColumns: string[] = ['Product', 'Created', 'Status', 'Rating','price','Feactured','Actions'];
-  dataSource!: MatTableDataSource<any>; ;
- 
- TodaysDate=new Date()
- constructor(private productlistservice:AllproductserviceService,private route:Router,public dialog: MatDialog) {
+  displayedColumns: string[] = [
+    'Product',
+    'Created',
+    'Status',
+    'Rating',
+    'price',
+    'Feactured',
+    'Actions',
+  ];
+  dataSource!: MatTableDataSource<any>;
 
-  }
-//  ngOnInit() {
-    
-//     this.productlistadded()
-    
-//   }
-  // productlistadded(){
-  //   this.productlistservice.GetDataToServer("product").subscribe((el:any)=>{
-  //     console.log(el)
-  //     this.dataSource=new MatTableDataSource(el);;
-      
-  //   })
-  // }
-
-
- 
-
-
-//  OnDelete(rowData: any) 
-//   {
-//     alert("Do you really want to delete  ? Category will be Deleted permanently.")
-   
-//      // console.log(rowData);
-//     let selectedIndex = this.dataSource.data.findIndex((el: any) => el.id === rowData.id);
-//     console.log("selectedIndex", selectedIndex);
-   
-//     const endpoint = "product/" + rowData.id;
-//     this.productlistservice.DeleteDataToServer(endpoint).subscribe((res: any) => 
-//     {
-//       console.log(res);
-    
-//       this.dataSource.data.splice(selectedIndex, 1);
-//       console.log("product Deleted SucessFully...!", this.dataSource);
-
-//       alert(" Deleted SucessFully...!");
-//       this.dataSource._updateChangeSubscription();
-//     },
-//       error => 
-//       {
-//         console.log(" product not deleted....!!")
-//       })
-
-//     }
- 
-
-
-
-//   dataSource!: MatTableDataSource<any>;
-
-//   TodaysDate = new Date();
-//   constructor(
-//     private productlistservice: ProductService,
-//     private route: Router,
-//     public dialog: MatDialog
-//   ) {}
+  TodaysDate = new Date();
+  constructor(
+    private productlistservice: AllproductserviceService,
+    private route: Router,
+    public dialog: MatDialog
+  ) {}
 
   ngOnInit() {
     this.productlistadded();
@@ -109,4 +65,3 @@ export class ProductListComponent {
     );
   }
 }
-
